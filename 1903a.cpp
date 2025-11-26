5492e3a9a715a94c22444ff556ead779a9f5fba8
Dharshan2208 <dharshan080622@gmail.com>
2025-11-27 00:55:59 +0530

    feat : 1903a optimised the sorted checking

--- 1903a.cpp
+++ 1903a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -11,17 +12,25 @@ int main() {
         vector<int> a(n);
         for (int i = 0; i < n; i++) cin >> a[i];
 
+        // Manual checking for sorting
+        // if (k > 1) {
+        //     cout << "YES\n";
+        // } else {
+        //     bool sorted = true;
+        //     for (int i = 1; i < n; i++) {
+        //         if (a[i] < a[i-1]) {
+        //             sorted = false;
+        //             break;
+        //         }
+        //     }
+        //     cout << (sorted ? "YES\n" : "NO\n");
+        // }
+
+        // Using in built
         if (k > 1) {
             cout << "YES\n";
         } else {
-            bool sorted = true;
-            for (int i = 1; i < n; i++) {
-                if (a[i] < a[i-1]) {
-                    sorted = false;
-                    break;
-                }
-            }
-            cout << (sorted ? "YES\n" : "NO\n");
+            cout << (is_sorted(a.begin(), a.end()) ? "YES\n" : "NO\n");
         }
     }
     return 0;
