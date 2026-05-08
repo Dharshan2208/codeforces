280a18970e6022249d189b2908ac8e41a55ad224
Dharshan2208 <dharshan080622@gmail.com>
2026-05-08 16:10:52 +0530

    feat : updated the cf.sh and improved the template for better coding ehhehe

--- cf.sh
+++ cf.sh
@@ -18,13 +18,45 @@ using namespace std;
 
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL)
 
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+/* Input */
+#define in(x) cin >> x
+#define vin(v,n) vector<int> v(n); for(auto &it : v) cin >> it
+#define vllin(v,n) vector<ll> v(n); for(auto &it : v) cin >> it
+
+/* Output */
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+void solve() {
+
+}
+
 int main() {
     fastio;
 
     int t;
     cin >> t;
     while (t--) {
-        // solve here
+        solve();
     }
 
     return 0;
